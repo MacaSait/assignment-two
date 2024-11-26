918da09de45ed7fbb830ea1aa53156c739baa895
MacaSait <macarena.perezcobo@edu.sait.ca>
2024-11-25 19:27:16 -0700

    Add files via upload

---
+++ story.js
@@ -0,0 +1,29 @@
+seventeen.addEventListener("click",function(){
+    console.log("seven");
+    document.getElementById("story_image").src="picture2.jpg"; 
+    story_period.innerHTML="17th Century";
+    story_description.innerHTML= "While there is no written confirmation of when or how was discovered. Jaques Cartier, who discovered Canada, was also the first European to write about the sugar maple and maple sap. In 1557 in one of his trips to Canada, Cartier cut a tree and tried the sweet-tasting sap. The first nations Habitants, who been eating this sap for many years before the arrive of the Europeans, told Cartier that they call it  the magical tree “couton”.The 17th century saw the early use of maple sugar, with indigenous knowledge shaping its production and consumption. Exportation also began during this period."
+    
+    })
+
+eighteen.addEventListener("click",function(){
+    console.log("eight");
+    document.getElementById("story_image").src="picture3.jpg";
+    story_period.innerHTML="18th Century";
+    story_description.innerHTML="18ths slow evolution in the use of maple sugar. And start the beginning of exportation" ;
+    })
+
+ nineteen.addEventListener("click",function(){
+    console.log("nine");
+    document.getElementById("story_image").src="picture4.jpg"; 
+    story_period.innerHTML="19th Century";
+    })
+    
+
+twenty.addEventListener("click",function(){
+    console.log("twenty");
+    document.getElementById("story_image").src="picture1.jpg"; 
+    story_period.innerHTML="20s Century";
+    })
+
+
